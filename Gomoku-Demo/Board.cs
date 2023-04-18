4535ca8b90bf04d3ff00aee74f5ece579f5aec8d
Your Name <you@example.com>
2023-04-18 23:51:04 +0800

    1st commit

---
+++ Gomoku-Demo/Board.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace Gomoku_Demo
+{
+    class Board
+    {
+        private static readonly int OFFSET = 75;
+        private static readonly int NODE_RADIUS = 5;
+        private static readonly int NODE_DISTANCE = 75;
+        private static readonly Point NO_MATCH_NODE = new Point(-1, -1);
+        public static readonly int NODE_COUNT=9;
+        private Piece[,] pieces = new Piece[NODE_COUNT, NODE_COUNT];
+        private Point lastPlacedNode = NO_MATCH_NODE;
+        public Point LastPlaceNode { get { return lastPlacedNode; } }
+        public PieceType GetPieceType(int nodeIdX,int nodeIdY)
+        {
+            if (pieces[nodeIdX, nodeIdY] == null)
+                return PieceType.NONE;
+            else
+                return pieces[nodeIdX, nodeIdY].GetPieceType();
+        }
+        public bool CanBePlace(int x, int y)
+        {
+            //TODO:找出最近節點
+            Point nodeId = FindTheCloseNode(x, y);
+            //TODO:如果沒有，回傳false
+            if (nodeId == NO_MATCH_NODE)
+                return false;
+
+            
+            //TODO:如果有，檢查是否有旗子存在
+            if (pieces[nodeId.X, nodeId.Y] != null)
+            {
+                return false;
+            }
+            return true;
+        }
+        public Piece PlaceAPiece(int x,int y,PieceType type)
+        {
+            //TODO:找出最近節點
+            Point nodeId = FindTheCloseNode(x, y);
+            //TODO:如果沒有，回傳false
+            if (nodeId == NO_MATCH_NODE)
+                return null;
+
+            //TODO:如果有，檢查是否有旗子存在
+            if (pieces[nodeId.X, nodeId.Y] != null)
+            {
+                return null;
+            }
+            //TODO:根據type產生對應旗子
+            Point formPos = convertToFormPosition(nodeId);
+            if (type == PieceType.BLACK)
+                pieces[nodeId.X, nodeId.Y] = new BlackPiece(formPos.X,formPos.Y);
+            else if(type==PieceType.WHITE)
+                pieces[nodeId.X, nodeId.Y] = new WhitePiece(formPos.X, formPos.Y);
+            //紀錄最後下子位置
+            lastPlacedNode = nodeId;
+
+            return pieces[nodeId.X, nodeId.Y];
+        }
+
+        private Point convertToFormPosition(Point nodId)
+        {
+            Point formPostion = new Point();
+            formPostion.X = nodId.X * NODE_DISTANCE + OFFSET;
+            formPostion.Y = nodId.Y * NODE_DISTANCE + OFFSET;
+            return formPostion;
+        }
+        private Point FindTheCloseNode(int x,int y)
+        {
+            int nodeIdX = FindTheClosetNode(x);
+            if (nodeIdX == -1 || nodeIdX>=NODE_COUNT)
+                return NO_MATCH_NODE;
+            int nodeIdY = FindTheClosetNode(y);
+            if (nodeIdY == -1||nodeIdY>=NODE_COUNT)
+                return NO_MATCH_NODE;
+            return new Point(nodeIdX, nodeIdY);
+        }
+        private int FindTheClosetNode(int pos)
+        {
+            pos -= OFFSET;
+            int quotient = pos / NODE_DISTANCE;
+            int remainder = pos % NODE_DISTANCE;
+            if (remainder <= NODE_RADIUS)
+                return quotient;
+            else if (remainder >= NODE_DISTANCE - NODE_RADIUS)
+                return quotient + 1;
+            else
+                return -1;
+        }
+    }
+}
---
+++ Gomoku-Demo/Game.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gomoku_Demo
+{
+    class Game
+    {
+        private Board board = new Board();
+        private PieceType currentPlayer = PieceType.BLACK;
+        private PieceType winner = PieceType.NONE;
+        private int countA = 0, countB = 0, countC = 0, countD = 0;
+
+
+        public PieceType Winner { get { return winner; } }
+        
+        public bool CanBePlaced(int x,int y)
+        {
+            return board.CanBePlace(x, y);
+        }
+       
+        public Piece PlaceAPiece(int x,int y)
+            {
+                Piece piece = board.PlaceAPiece(x,y, currentPlayer);
+                if (piece != null)
+                {
+                    //檢查選手獲勝
+                    CheckWinner();
+
+                    //交換選手
+                    if (currentPlayer == PieceType.BLACK)
+                        currentPlayer = PieceType.WHITE;
+                    else if (currentPlayer == PieceType.WHITE)
+                        currentPlayer = PieceType.BLACK;
+                    return piece;
+                }
+                return null;
+            }
+        private void CheckWinner()
+        {
+            int centerX = board.LastPlaceNode.X;
+            int centerY = board.LastPlaceNode.Y;
+            
+
+            //檢查八個方向
+            for (int xDir = -1; xDir <= 1; xDir++)
+            {
+                for(int yDir = -1; yDir <= 1; yDir++)
+                {
+                    //排除中間的情況
+                    if (xDir == 0 && yDir == 0)
+                        continue;
+                    //紀錄現在看到幾顆相同棋子
+                    int count = 1;
+                   
+                    while (count < 5)
+                    {
+                        //設定被檢查的目標
+                        int targetX = centerX + count * xDir;
+                        int targetY = centerY + count * yDir;
+
+
+                        //超出棋盤 或是顏色與當前棋子不同色 如果不同顏色 就跳出 如果相同 count++
+                        if (targetX < 0 || targetX >= Board.NODE_COUNT ||
+                            targetY < 0 || targetY >= Board.NODE_COUNT ||
+                            board.GetPieceType(targetX, targetY) != currentPlayer)
+                        //不論檢查有多少 都先記錄 分別為 (-1-1 11) (-11  1-1)   (-10 10) (01)(0-1)共四組
+                        {
+                            break;
+                        }
+
+                        
+                        count++;
+                    }
+                    
+                    if ((xDir < 0 && yDir < 0) || (xDir > 0 && yDir > 0))
+                    {
+                        countA+= count;
+                    }
+                    if ((xDir < 0 && yDir > 0) || (xDir > 0 && yDir < 0))
+                    {
+                        countB+= count;
+                    }
+                    if ((xDir < 0 && yDir == 0) || (xDir > 0 && yDir == 0))
+                    {
+                        countC+=count;
+                    }
+                    if ((xDir == 0 && yDir > 0) || (xDir == 0 && yDir < 0))
+                    {
+                        countD+=count;
+                    }
+
+                    //檢查是否看到五顆棋子
+                    if (countA >= 6||countB >= 6 ||countC >= 6 ||countD >= 6)
+                    {
+                        winner = currentPlayer;
+                    }
+                    //if (count == 5)
+                    //{
+                    //    winner = currentPlayer;
+                    //}
+
+                   
+
+                }
+            }
+            countA = 0;
+            countB = 0;
+            countC = 0;
+            countD = 0;
+        }
+    }
+}
